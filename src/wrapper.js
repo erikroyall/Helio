b0b95b9131468dc03f386847778494bfdae3fa07
Erik Royall <erikroyall@hotmail.com>
2013-08-19 12:24:32 +0530

    Add wrapper constructor functions

---
+++ src/wrapper.js
@@ -0,0 +1,20 @@
+    
+  // --------------------------------------------------
+  // Helio Internal Class' wrappers
+  // --------------------------------------------------
+
+  function HelioObject (obj) {
+    this.o = obj;
+  }
+
+  function HelioString (str) {
+    this.s = str;
+  }
+
+  function HelioArray (arr) {
+    this.a = arr;
+  }
+
+  function HelioNumber (number) {
+    this.n = number;
+  }
