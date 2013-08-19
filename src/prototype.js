901791a1faecc9e588591f0da74d9dce9b9879bb
Erik Royall <erikroyall@hotmail.com>
2013-08-19 12:57:13 +0530

    Small fixes

--- src/prototype.js
+++ src/prototype.js
@@ -3,7 +3,7 @@
   // Prototypes of internal objects
   // --------------------------------------------------
 
-  extend(HelioObject.prototype, {
+  Helio.extend(HelioObject.prototype, {
     isArray: false,
     isString: false,
     isNumber: false,
@@ -12,20 +12,53 @@
     isObject: true,
 
     extend: function (ext) {
-      return extend.call(this, this.o, ext);
+      return Helio.extend.call(this, this.o, ext);
     }
   });
 
-  extend(HelioString.prototype, {
+  Helio.extend(HelioString.prototype, {
+    isArray: false,
+    isString: true,
+    isNumber: false,
+    isBoolean: false,
+    isNaN: false,
+    isObject: false,
+
     isEmpty: function () {
       return this.s.length > 0;
     },
 
     contains: function (str) {
-      
+      return this.s.indexOf(str) > -1;
     }
   });
 
-  extend(HelioArray.prototype, {});
+  Helio.extend(HelioArray.prototype, {
+    isArray: true,
+    isString: false,
+    isNumber: false,
+    isBoolean: false,
+    isNaN: false,
+    isObject: false,
 
-  extend(HelioNumber.prototype, {});
+    each: function (fn) {
+      return Helio.each(this.a, fn);
+    },
+
+    map: function (fn) {
+      return Helio.map(this.a, fn);
+    }
+  });
+
+  Helio.extend(HelioNumber.prototype, {
+    isArray: false,
+    isString: false,
+    isNumber: true,
+    isBoolean: false,
+    isNaN: false,
+    isObject: false,
+
+    times: function (fn) {
+      Helio.times(this.n, fn);
+    }
+  });
