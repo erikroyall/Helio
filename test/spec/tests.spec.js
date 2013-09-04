f819d422086947865514a45231bcc0f6771613b8
Erik Royall <erikroyall@hotmail.com>
2013-09-04 06:00:56 +0530

    Add test utils

--- test/spec/tests.spec.js
+++ test/spec/tests.spec.js
@@ -1,12 +1,14 @@
+var _ = Helio;
+
 describe(".each()", function () {
   it("should fail silently when passed with empty array", function() {
-    expect(Helio.each([], function (e) {})).toEqual(void 0);
+    expect(_.each([], function (e) {})).toEqual(void 0);
   });
   it("should handle one element arrays", function () {
     var arr = ["one"]
       , val = "";
 
-    Helio.each(["one"], function (e) {
+    _.each(["one"], function (e) {
       val += e;
     });
 
@@ -15,10 +17,25 @@ describe(".each()", function () {
   it("should handle multiple elements", function() {
     var val = "";
 
-    Helio.each(["helio", "wald"], function (e) {
+    _.each(["helio", "wald"], function (e) {
       val += e.toUpperCase();
     });
 
     expect(val).toEqual("HELIOWALD");
+  });
+});
+describe(".pluck()", function() {
+  it("it should handle empty arrays", function () {
+    expect(_.pluck([], "name")).toEqual([]);
+  });
+  it("should get the keys of the array", function () {
+    var arr = [
+      {name:"Erik Royall", age: 14},
+      {name:"John Doe", age: 30},
+      {name:"Jane Doe", age: 27}
+    ];
+
+    expect(_.pluck(arr, "name")).toEqual(["Erik Royall", "John Doe", "Jane Doe"]);
+    expect(_.pluck(arr, "age")).toEqual([14, 30, 27]);
   });
 });
